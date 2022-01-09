7c150afa85ce2f736bd54e7f095a87a90da328a0
sovni <christophe.burgalat@gmail.com>
2022-01-09 18:16:19 +0100

    Fix base Path issue for github pages

--- app-vote/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-      ? '/gh-vote-dapp/'
-      : '/',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/gh-vote-dapp/'
-      : '/'
-  }
